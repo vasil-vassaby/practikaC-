976eafae1f9b352f01185bec244d5c00b6a1a15d
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-10-13 11:50:52 +0300

    add task01

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая 
+//1. на вход принимает число
+//2. и выдает его квадрат
+
+//Например:
+//4 -> 16
+Console.Clear();
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); //переводит строку в число, в данном случае
+Console.Write($"Квадрат числа {number} = {number*number}"); //позволяет включить переменные в текст вывода
