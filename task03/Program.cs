e8ca276a69be06a7b86d3e372cf355af78a48ac7
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-10-14 11:42:16 +0300

    changed task03

--- task03/Program.cs
+++ task03/Program.cs
@@ -5,35 +5,11 @@
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 1)
-{
-    Console.Write("Понедельник");
-}
-else if (number == 2)
-{
-    Console.Write("Вторник");
-}
-else if (number == 3)
-{
-    Console.Write("Среда");
-}
-else if (number == 4)
-{
-    Console.Write("Четверг");
-}
-else if (number == 5)
-{
-    Console.Write("Пятница");
-}
-else if (number == 6)
-{
-    Console.Write("Суббота");
-}
-else if (number == 7)
-{
-    Console.Write("Воскресенье");
-}
-else 
-{
-    Console.Write("Не верно");
-}
+if (number == 1) Console.Write("Понедельник");
+else if (number == 2) Console.Write("Вторник");
+else if (number == 3) Console.Write("Среда");
+else if (number == 4) Console.Write("Четверг");
+else if (number == 5) Console.Write("Пятница");
+else if (number == 6) Console.Write("Суббота");
+else if (number == 7) Console.Write("Воскресенье");
+else Console.Write("В неделе только 7 дней. Введите число от 1 до 7!");
