b24e8d6374490a3b0d518be80b9a1ea60d631fcb
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-10-14 09:40:19 +0300

    changed task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -12,11 +12,6 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
-int count = 0;
-while (count < 1)
-{
-    if (max < number2) max = number2;
-    if (max < number3) max = number3;
-    Console.WriteLine($"Максимальное из 3-х чисел = {max}");
-    count++;
-}
+if (max < number2) max = number2;
+if (max < number3) max = number3;
+Console.WriteLine($"Максимальное из 3-х чисел = {max}");
