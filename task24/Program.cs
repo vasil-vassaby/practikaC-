96e67d218abbbe5781f350da753ce24a116768db
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-11-07 11:03:28 +0300

    add task24

---
+++ task24/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа: принимает на ввод число А и выводит сумму данного числа от 1 до А
+//7 -> 28
+
+Console.Clear();
+Console.Write("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num) 
+{
+    int sum = default;
+    for (int i = 1; i <= num; i++) sum += i;
+    return sum;
+}
+
+int sumNumbers = SumNumbers(number);
+ Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
