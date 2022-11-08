0b307f53040ba1a035e6d80840025790cb5af5e3
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-11-08 13:09:08 +0300

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите цикл, который 
+// принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int NumberInPower(int numA, int numB)
+{
+    int count = 0;
+    int numC = 1;
+    while (count < numB)
+    {
+        numC = numC * numA;
+        count++;
+    }
+    return numC;
+}
+
+if (numberB >= 0)
+{
+    int numberInPower = NumberInPower(numberA, numberB);
+    Console.WriteLine($"Число {numberA} в натуральной степени {numberB} = {numberInPower}");
+}
+else Console.WriteLine("Введите натуральное число B!");
