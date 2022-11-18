d79fafae3e3e7acfd3bdbfa1d607327334e8e410
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-11-18 10:12:27 +0300

    added task52

--- task52/Program.cs
+++ task52/Program.cs
@@ -6,3 +6,64 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],2} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] MeanArithmeticElementsColumns(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    double[] array = new double[columns];
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double mean = default;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            mean = mean + matrix[i, j] / (double)rows;
+        }
+        array[j] = Math.Round(mean, 1);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write($"Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(i < array.Length - 1 ? $"{array[i]}; " : $"{array[i]}");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+
+double[] meanArithmeticElementsColumns = MeanArithmeticElementsColumns(array2D);
+PrintArray(meanArithmeticElementsColumns);
