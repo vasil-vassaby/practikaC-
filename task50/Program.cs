99b0c286cf3ec58d81daa8843a669e9b4b11e4c0
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-11-18 11:19:54 +0300

    added task50

--- task50/Program.cs
+++ task50/Program.cs
@@ -5,24 +5,26 @@
 // Например, задан массив:
 
 // 1 4 7 2
-
 // 5 9 2 3
-
 // 8 4 2 4
-
 // 1, 7 -> такого элемента в массиве нет
+
+Console.Write("Введите значение позиции элемента в строке: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение позиции элемента в столбце: ");
+int j = Convert.ToInt32(Console.ReadLine());
 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
     int[,] matrix = new int[rows, columns];
     Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(0); i++) //строка - matrix.GetLength(0) по условию 3
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++) //столбец - matrix.GetLength(1) по условию 4
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i,j] = rnd.Next(min, max + 1);
-        } 
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
     }
     return matrix;
 }
@@ -34,11 +36,22 @@ void PrintMatrix(int[,] matrix)
         Console.Write("|");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-        Console.Write($"{matrix[i,j], 5} ");
+            Console.Write($"{matrix[i, j],2} ");
         }
         Console.WriteLine("|");
     }
 }
 
-int[,] array2D = CreateMatrixRndInt(3, 4, -100, 150);
+int SearchingElementArray(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    if (i < rows && j < columns) return matrix[i,j];
+    else  return 0;
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
 PrintMatrix(array2D);
+
+int result = SearchingElementArray(array2D);
+Console.WriteLine(result == 0 ? "Такого элемента в массиве нет!" : $"Элемент равен {result}");
